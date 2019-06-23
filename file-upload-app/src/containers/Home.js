b7df4d14de02eec7002fe160425c997d9ae28095
Richa Sharma <onlinericha19@gmail.com>
2019-06-23 23:28:33 +0530

    added homepage

---
+++ file-upload-app/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Upload Your Files</h1>
+          <p>A simple app to organize your app</p>
+        </div>
+      </div>
+    );
+  }
+}
